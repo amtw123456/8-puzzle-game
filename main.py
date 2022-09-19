5a683e659b5a53257b42b7883ac86b2d376fec55
amtw123456 <radolor@up.edu.ph>
2022-09-19 23:52:16 +0800

    Added Grid, Tile class and their methods drawGrid, drawTile and drawTiles, added split function and modified draw function and changes in main function

--- main.py
+++ main.py
@@ -11,37 +11,84 @@ COLOR_WHITE= (255, 255, 255)
 
 pygame.display.set_caption("8-Puzzle Game")
 
-def drawGrid(gridDistance, rows, surface):
-    sizeBtwn = gridDistance // rows
+class Grid(object):
+    def __init__(self, surface, tiles, rows, gridSize):
+        self.gridSize = gridSize
+        self.surface = surface
+        self.tiles = tiles
+        self.rows = rows
 
-    x = 0
-    y = 0
+    def drawGrid(self):
+        sizeBtwn = self.gridSize // self.rows
 
-    for l in range(rows):
-        x = x + sizeBtwn
-        y = y + sizeBtwn
+        x = 0
+        y = 0
 
-        pygame.draw.line(surface, COLOR_BLACK, (x, 0), (x, gridDistance))
-        pygame.draw.line(surface, COLOR_BLACK, (0, y), (gridDistance, y))
+        for l in range(self.rows):
+            x = x + sizeBtwn
+            y = y + sizeBtwn
 
-def draw(gridDistance, rows, surface):
+            pygame.draw.line(self.surface, COLOR_BLACK, (x, 0), (x, self.gridSize))
+            pygame.draw.line(self.surface, COLOR_BLACK, (0, y), (self.gridSize, y))
+
+    def drawTiles(self):
+        for i in range(len(self.tiles)):
+            if self.tiles[i] == None:
+                continue
+            else:
+                self.tiles[i].drawTile(self.surface)
+
+
+class Tile(object):
+    def __init__(self, posX, posY, sizeOfTile, tileNumber):
+        self.posX = posX
+        self.posY = posY
+        self.sizeOfTile = sizeOfTile
+        self.tileNumber = tileNumber
+        self.tileImage = pygame.transform.scale(pygame.image.load(
+            os.path.join('Assets', 'number' + str(tileNumber) + '.png')), (sizeOfTile, sizeOfTile))
+
+    def drawTile(self, surface):
+        surface.blit(self.tileImage, (self.posX, self.posY))
+
+
+def draw(surface, gridDistance, rows, tiles, GRID):
     surface.fill(COLOR_WHITE)
-    drawGrid(gridDistance, rows, surface)
-
+    GRID.drawGrid()
+    GRID.drawTiles()
     pygame.display.update()
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
 
 def main():
 
     rows = 3
     size = WINDOW_SCREEN_WIDTH
     distanceBetweenGrids = WINDOW_SCREEN_WIDTH // rows
-    print(distanceBetweenGrids)
+    tileSize = distanceBetweenGrids
     window = pygame.display.set_mode((WINDOW_SCREEN_WIDTH*2, WINDOW_SCREEN_HEIGHT))
+    arrayOfNumbers = list(split(random.sample(range(0, 9), 9), 3))
+    # arrayOfNumbers = [[1,2,3],[4,5,6],[7,0,8]]
+    print(arrayOfNumbers)
+    correctPos = [[1,2,3],[4,5,6],[7,8,0]]
+    tiles = []
+
+    for i in range(rows):
+        for j in range(rows):
+            if arrayOfNumbers[i][j] == 0:
+                tiles.append(None)
+            else:
+                tiles.append(Tile(tileSize * j, tileSize * i, tileSize, arrayOfNumbers[i][j]))
+    print(tiles)
+
+    GRID = Grid(window, tiles, rows, size)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-        draw(size, rows, window)
+        draw(window, size, rows, tiles, GRID)
 
 main()
