fa0ecc03844b1bcb4c30f4c3cd11e063ab77413d
amtw123456 <radolor@up.edu.ph>
2022-09-20 01:33:10 +0800

    Modified Tile and Grid Methods and main() function

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ WINDOW_SCREEN_WIDTH = 510
 WINDOW_SCREEN_HEIGHT = 510
 
 COLOR_BLACK = (0, 0, 0)
-COLOR_WHITE= (255, 255, 255)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GRAY = (230, 230, 230)
 
 pygame.display.set_caption("8-Puzzle Game")
 
@@ -36,26 +37,78 @@ class Grid(object):
             if self.tiles[i] == None:
                 continue
             else:
-                self.tiles[i].drawTile(self.surface)
+                self.tiles[i].drawTile()
+
+    def moveTiles(self):
+        for i in range(len(self.tiles)):
+            if self.tiles[i] == None:
+                continue
+            else:
+                self.tiles[i].moveTile()
 
 
 class Tile(object):
-    def __init__(self, posX, posY, sizeOfTile, tileNumber):
+    def __init__(self, posX, posY, sizeOfTile, tileNumber, surface):
         self.posX = posX
         self.posY = posY
+        self.surface = surface
         self.sizeOfTile = sizeOfTile
         self.tileNumber = tileNumber
+        self.rect = pygame.draw.rect(surface, (0, 0, 0), (posX, posY, sizeOfTile, sizeOfTile))
         self.tileImage = pygame.transform.scale(pygame.image.load(
             os.path.join('Assets', 'number' + str(tileNumber) + '.png')), (sizeOfTile, sizeOfTile))
 
-    def drawTile(self, surface):
-        surface.blit(self.tileImage, (self.posX, self.posY))
 
+    def drawTile(self):
+        self.surface.blit(self.tileImage, (self.posX, self.posY))
+
+    def moveTile(self, direction):
+        if(direction == 0):
+            for i in range(34):
+                self.posY += 5
+                self.rect.y += 5
+                time.sleep(0.02)
+                self.surface.blit(self.tileImage, (self.posX, self.posY))
+                pygame.display.update()
+        if(direction == 1):
+            for i in range(34):
+                self.posY -= 5
+                self.rect.y -= 5
+                time.sleep(0.02)
+                self.surface.blit(self.tileImage, (self.posX, self.posY))
+                pygame.display.update()
+        if(direction == 2):
+            for i in range(34):
+                self.posX += 5
+                self.rect.X += 5
+                time.sleep(0.02)
+                self.surface.blit(self.tileImage, (self.posX, self.posY))
+                pygame.display.update()
+        if(direction == 3):
+            for i in range(34):
+                self.posX -= 5
+                self.rect.X -= 5
+                time.sleep(0.02)
+                self.surface.blit(self.tileImage, (self.posX, self.posY))
+                pygame.display.update()
+
+        # self.rect.y += 5
+
+    def tileClicked(self, cursorPosX, cursorPosY):
+        if(self.rect.collidepoint((cursorPosX, cursorPosY))):
+            # print("Tile:", self.tileNumber)
+            self.moveTile(0)
+            return self.tileNumber
+        # if ((self.sizeOfTile * counterX) < cursorPosX and (self.sizeOfTile * counterY) < cursorPosY) and (
+        #     (self.sizeOfTile * counterX) < cursorPosX and ((self.sizeOfTile * counterY) + self.sizeOfTile) > cursorPosY) and (
+        #     ((self.sizeOfTile * counterX)  + self.sizeOfTile) > cursorPosX and (self.sizeOfTile * counterY) < cursorPosY) and (
+        #     ((self.sizeOfTile * counterX)  + self.sizeOfTile) > cursorPosX and ((self.sizeOfTile * counterY) + self.sizeOfTile) > cursorPosY):
 
 def draw(surface, gridDistance, rows, tiles, GRID):
-    surface.fill(COLOR_WHITE)
+    surface.fill(COLOR_GRAY)
     GRID.drawGrid()
     GRID.drawTiles()
+    # GRID.moveTiles()
     pygame.display.update()
 
 def split(a, n):
@@ -63,14 +116,14 @@ def split(a, n):
     return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
 
 def main():
-
+    clock = pygame.time.Clock()
     rows = 3
     size = WINDOW_SCREEN_WIDTH
     distanceBetweenGrids = WINDOW_SCREEN_WIDTH // rows
     tileSize = distanceBetweenGrids
     window = pygame.display.set_mode((WINDOW_SCREEN_WIDTH*2, WINDOW_SCREEN_HEIGHT))
-    arrayOfNumbers = list(split(random.sample(range(0, 9), 9), 3))
-    # arrayOfNumbers = [[1,2,3],[4,5,6],[7,0,8]]
+    # arrayOfNumbers = list(split(random.sample(range(0, 9), 9), 3))
+    arrayOfNumbers = [[1,2,3],[4,5,6],[7,0,8]]
     print(arrayOfNumbers)
     correctPos = [[1,2,3],[4,5,6],[7,8,0]]
     tiles = []
@@ -80,15 +133,25 @@ def main():
             if arrayOfNumbers[i][j] == 0:
                 tiles.append(None)
             else:
-                tiles.append(Tile(tileSize * j, tileSize * i, tileSize, arrayOfNumbers[i][j]))
+                tiles.append(Tile(tileSize * j, tileSize * i, tileSize, arrayOfNumbers[i][j], window))
     print(tiles)
 
     GRID = Grid(window, tiles, rows, size)
 
     while True:
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                cursorPosX, cursorPosY = pygame.mouse.get_pos()
+                for i in range(len(tiles)):
+                    if tiles[i] == None:
+                        continue
+                    else:
+                        tiles[i].tileClicked(cursorPosX, cursorPosY)
+
+
         draw(window, size, rows, tiles, GRID)
 
 main()
